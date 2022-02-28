ffc8aedbf4d85228a83a70cc2d8a8f9e666c8c22
Mona Lisa <ctukraine22@gmail.com>
2022-02-28 18:47:40 +0200

    new

--- runner.sh
+++ runner.sh
@@ -2,19 +2,15 @@
 
 run()
 {
-    vpn_code=$1
-    B_Command=$2
+    export VPN_CODE=$1
+    export B_TARGET=$2
     for i in {1..60}
     do
-        echo "Running $i time. $B_Command"
+        echo "Running $i time. $VPN_CODE $B_TARGET"
         target=$1
         sudo docker-compose up -d --force-recreate
+        echo "Sleeping"
         sleep 60s
         sudo docker-compose down
     done
-    
-}
-
-runT() {
-    echo "Hello $1"
 }
