5746c5e65dbd1cdb31bbe78a2e0a27865f683874
Mona Lisa <ctukraine22@gmail.com>
2022-02-28 20:08:18 +0200

    new

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@ run()
     do
         echo "Running $i time. $VPN_CODE $B_TARGET"
         sudo -E docker-compose up -d --force-recreate
-        echo "Running"
+        echo "Executing..."
         sleep 60s
         sudo -E docker-compose down
     done
