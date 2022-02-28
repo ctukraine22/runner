7d3f14c43bbff65b25f828bb8c732fbd51b6b28f
Mona Lisa <ctukraine22@gmail.com>
2022-02-28 22:08:15 +0200

    x

--- expressvpn/runner.sh
+++ expressvpn/runner.sh
@@ -37,3 +37,19 @@ ddosripper()
         sudo -E docker-compose down
     done
 }
+checksites(){
+    export VPN_CODE=$1
+    export VPN_COUNTRY=$2
+    for i in {1..60}
+    do
+        echo "Running bombardier $i time. $VPN_CODE $VPN_COUNTRY"
+        sudo -E docker-compose down
+        sudo -E docker-compose up -d --force-recreate vpn 
+        sleep 10s
+        sudo -E docker-compose run test
+        sudo -E docker-compose run -d checksites
+        echo "Executing..."
+        sleep 120s
+        sudo -E docker-compose down
+    done
+}
