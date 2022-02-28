e09683fd91a46633926cfd327f3d96b60a06b007
Mona Lisa <ctukraine22@gmail.com>
2022-02-28 20:31:35 +0200

    x

--- bombardier/runner.sh
+++ bombardier/runner.sh
@@ -8,6 +8,7 @@ run()
     do
         echo "Running $i time. $VPN_CODE $B_TARGET"
         sudo -E docker-compose up -d --force-recreate vpn 
+        sleep 10s
         sudo -E docker-compose run test
         sudo -E docker-compose run bombardier
         echo "Executing..."
