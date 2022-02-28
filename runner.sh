bc328c84dcd3acb2430640649ca6dfd2680a7dd7
Mona Lisa <ctukraine22@gmail.com>
2022-02-28 15:07:12 +0200

    туц

---
+++ runner.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+run()
+{
+    vpn_code=$1
+    B_Command=$2
+    for i in {1..60}
+    do
+        echo "Running $i time. $B_Command"
+        target=$1
+        docker-compose up -d --force-recreate
+        sleep 60s
+        docker-compose down
+    done
+    
+}
