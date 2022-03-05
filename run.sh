ffec9de070e76ed732387767e0867cf13b263741
Mona Lisa <ctukraine22@gmail.com>
2022-03-05 14:59:26 +0200

    x

--- run.sh
+++ run.sh
@@ -45,8 +45,10 @@ run(){
     start_vpn
     sudo rm -f "$tool.log"
     screen -dm -S tool -L -Logfile "$tool.log" sudo -E docker-compose up $tool
+    screen -ls
     while true
     do
+        echo "Last logs on $(date):"
         tail -n 10 "./$tool.log"
         sleep 30s
     done
