86b8f42c4f6549b4d4a8240f41b26b674a08b6dc
Mona Lisa <ctukraine22@gmail.com>
2022-03-06 13:31:55 +0200

    X

--- run.sh
+++ run.sh
@@ -44,7 +44,9 @@ uashield() {
 }
 bombardier()
 {
-    run "bombardier" "-k -H \"user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36\" $@"
+    bomb_args="-k -H \"user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36\" $@"
+    echo "bombardier arguments: $bomb_args"
+    run "bombardier" bomb_args
 }
 ddosripper()
 {
