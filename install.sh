595c3b03ab33166c9d8dab02234ddde1774c6dd5
Mona Lisa <ctukraine22@gmail.com>
2022-03-12 18:46:09 +0200

    x

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ else
         gnupg \
         lsb-release \
         vnstat \
-        net-tools
+        net-tools \
+        screen
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
